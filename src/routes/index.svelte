e58bd06ddf09fb56c4461b5452cdc5526a492f1d
Nick Noble <frontdesk@nicknoble.works>
2020-06-09 16:49:02 -0400

    init sapper

---
+++ src/routes/index.svelte
@@ -0,0 +1,40 @@
+<style>
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Guide Tools</title>
+</svelte:head>
+
+<h1>Guide Tools</h1>
+<p>Tools for running games with the <a href="https://www.adventure.game?ref=guide-tools">Quest</a> TTRPG game system.</p>
