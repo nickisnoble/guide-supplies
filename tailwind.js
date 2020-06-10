e53c9b04dfc2d28a098c425dbb59e0584a8a15c1
Nick Noble <frontdesk@nicknoble.works>
2020-06-10 10:51:13 -0400

    use tailwind

---
+++ tailwind.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: [],
+    fontFamily: {
+      display: ['Alegreya', 'serif'],
+      heading: ['"Alegreya Sans SC"', 'sans-serif'],
+      body: ['Ovo', 'serif'],
+    },
+    fontSize: {
+      xs:    '12px',
+      base:  '15px',
+      md:    '17px',
+      lg:    '20px',
+      xl:    '24px',
+      '2xl': '30px',
+      '3xl': '40px',
+      '4xl': '80px'
+
+    }
+  },
+  variants: {},
+  plugins: [],
+}
