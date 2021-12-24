7ecd491c5a4ac138d438f7d1a10de61c82cd0060
Nick Noble <nick.noble@hey.com>
2021-12-23 22:37:06 -0500

    add credit to footer

--- src/components/site/Footer.svelte
+++ src/components/site/Footer.svelte
@@ -1,10 +1,10 @@
 <footer class="p-4 bg-black text-white text-sm grid md:grid-cols-3 gap-4">
   <h1 class="text-xl">Guide Supplies</h1>
-  
+
   <div class="legal space-y-1">
     <p>Quest is a trademark of The&nbsp;Adventure&nbsp;Guild,&nbsp;LLC.</p>
-    <p>Guide Supplies (this website) is not affiliated with or endorsed by The&nbsp;Adventure&nbsp;Guild,&nbsp;LLC.</p>
-    <p>You should <a href="https://www.adventure.game/shop?ref=guide-supplies">buy the game</a>!</p>
+    <p>Guide Supplies (this website) is not affiliated with or endorsed by The&nbsp;Adventure&nbsp;Guild,&nbsp;LLC. You should <a href="https://www.adventure.game/shop?ref=guide-supplies">buy the game</a>!</p>
+    <p>This site is <a href="https://nicknoble.works/?ref=guide-supplies">Nick Noble</a>'s side project</p>
   </div>
 
   <div class="open-source space-y-1">
